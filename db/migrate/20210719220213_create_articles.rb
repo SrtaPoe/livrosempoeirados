48de5ee2336c3c3ef34d8cf67fb6b3ed241393ec
jhoaniceoliveira@gmail.com <jhoaniceoliveira@gmail.com>
2021-07-19 19:23:39 -0300

    Adicionando view atualizada

---
+++ db/migrate/20210719220213_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
+
+#nessa pagina eu posso incluir ou excluir campos da pagina
