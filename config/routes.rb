452be9de7a91a4930788ab6158584c7be4ca474e
jhoaniceoliveira@gmail.com <jhoaniceoliveira@gmail.com>
2021-07-20 19:13:06 -0300

    atualizando metodos de id

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'articles#index'
- get '/articles', to: 'articles#index'
- #inserindo rotas para useres
+  #inserindo rotas para useres
+
+  resources :articles
 end
