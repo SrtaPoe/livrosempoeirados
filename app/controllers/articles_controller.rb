00d7e41c89440191271deeee596229755e8e36db
jhoaniceoliveira@gmail.com <jhoaniceoliveira@gmail.com>
2021-07-30 16:29:50 -0300

    adicionando metodo destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def new
@@ -22,12 +24,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    
   end
 
   def update
-    @article = Article.find(params[:id])
-
+   
     if @article.update(article_params)
       redirect_to @article
     else
@@ -35,6 +36,11 @@ class ArticlesController < ApplicationController
     end
   end
 
+def destroy
+
+  @article.destroy
+  redirect_to root_path
+end
 
   private 
 
@@ -42,5 +48,7 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :body)
     #permite apenas os parametros que quero
   end
-
+  def set_article
+    @article = Article.find(params[:id])
+end
 end
